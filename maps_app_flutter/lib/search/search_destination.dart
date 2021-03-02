767eeca694ac04719e2e3cc3d3171d815b91a54e
Keny Ramírez <44176700+KBRR11@users.noreply.github.com>
2021-03-01 23:11:27 -0500

    debouncer to search places

--- maps_app_flutter/lib/search/search_destination.dart
+++ maps_app_flutter/lib/search/search_destination.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,100 +6,95 @@ import 'package:maps_app_flutter/model/search_response.dart';
 import 'package:maps_app_flutter/model/search_result.dart';
 import 'package:maps_app_flutter/services/traffic_service.dart';
 
-class SearchDestination extends SearchDelegate<SearchResult>{
-
+class SearchDestination extends SearchDelegate<SearchResult> {
   @override
   final String searchFieldLabel;
   final TrafficService _trafficService;
   final LatLng miUbicacion;
 
-  SearchDestination(
-    this.miUbicacion
-    ): this.searchFieldLabel = 'Buscar...',
-                       this._trafficService = new TrafficService();
-
+  SearchDestination(this.miUbicacion)
+      : this.searchFieldLabel = 'Buscar...',
+        this._trafficService = new TrafficService();
 
   @override
   List<Widget> buildActions(BuildContext context) {
-      return [
-        IconButton(icon: Icon(Icons.clear), onPressed: () => this.query = '')
-      ];
-    }
-  
-    @override
-    Widget buildLeading(BuildContext context) {
-      
-      return IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () => this.close(context, SearchResult(cancelo: true)));
-    }
-  
-    @override
-    Widget buildResults(BuildContext context) {
-      
-      return this._construirResultadosSugerencias();
-    }
-  
-    @override
-    Widget buildSuggestions(BuildContext context) {
-      if (this.query.length == 0){
-    return ListView(
-      children: [
-        ListTile(
-          leading: Icon(Icons.location_on),
-          title: Text('Colocar ubicación manualmente'),
-          onTap: (){
-           final busquedaBloc =  BlocProvider.of<BusquedaBloc>(context);
-           busquedaBloc.add(OnActivarManualMarker());
-            this.close(context, SearchResult(cancelo: false, manual: true));
-          },
-        )
-      ],
-    );
-
-      }
-      return this._construirResultadosSugerencias();
+    return [
+      IconButton(icon: Icon(Icons.clear), onPressed: () => this.query = '')
+    ];
   }
 
-  Widget _construirResultadosSugerencias(){
-  if (this.query.length == 0) {
-    return Container();
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        onPressed: () => this.close(context, SearchResult(cancelo: true)));
   }
 
-    return FutureBuilder(
-      future: _trafficService.getResultadosRequest(this.query.trim(), this.miUbicacion),
-      builder: (BuildContext context, AsyncSnapshot<SearchResponse> snapshot) { 
-            if (!snapshot.hasData) {
-              return Center(child: CircularProgressIndicator());
-            }
+  @override
+  Widget buildResults(BuildContext context) {
+    return this._construirResultadosSugerencias();
+  }
 
-            final lugares = snapshot.data.features;
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (this.query.length == 0) {
+      return ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text('Colocar ubicación manualmente'),
+            onTap: () {
+              final busquedaBloc = BlocProvider.of<BusquedaBloc>(context);
+              busquedaBloc.add(OnActivarManualMarker());
+              this.close(context, SearchResult(cancelo: false, manual: true));
+            },
+          )
+        ],
+      );
+    }
+    return this._construirResultadosSugerencias();
+  }
 
-            if (lugares.length==0) {
-              return ListTile(
-               leading: Icon(Icons.error_outline_outlined),
-               tileColor: Colors.red[300],
-               title: Text('No se encontraron resultados'),
-               subtitle: Text('intenta con algo diferente'),
-              );
-            }
+  Widget _construirResultadosSugerencias() {
+    if (this.query.length == 0) {
+      return Container();
+    }
+    this._trafficService.getSugerenciasPorQuery(this.query.trim(), this.miUbicacion);
 
-            return ListView.separated(
+    return StreamBuilder(
+        stream: this._trafficService.sugerenciasStream,
+        builder:
+            (BuildContext context, AsyncSnapshot<SearchResponse> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final lugares = snapshot.data.features;
+
+          if (lugares.length == 0) {
+            return ListTile(
+              leading: Icon(Icons.error_outline_outlined),
+              tileColor: Colors.red[300],
+              title: Text('No se encontraron resultados'),
+              subtitle: Text('intenta con algo diferente'),
+            );
+          }
+
+          return ListView.separated(
               itemCount: lugares.length,
-              separatorBuilder: (_, i)=>Divider(), 
-              itemBuilder: (_, i){
+              separatorBuilder: (_, i) => Divider(),
+              itemBuilder: (_, i) {
                 final lugar = lugares[i];
 
                 return ListTile(
                   leading: Icon(Icons.place),
                   title: Text(lugar.textEs),
                   subtitle: Text(lugar.placeNameEs),
-                  onTap: (){
-                   print(lugar.text);
+                  onTap: () {
+                    print(lugar.text);
                   },
                 );
-              } 
-              );
-       }
-      );
+              });
+        });
   }
-
 }
--- maps_app_flutter/lib/services/traffic_service.dart
+++ maps_app_flutter/lib/services/traffic_service.dart
@@ -1,9 +1,12 @@
 
 
 
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:maps_app_flutter/Secret%20Keys/keys.dart';
+import 'package:maps_app_flutter/helpers/debouncer.dart';
 import 'package:maps_app_flutter/model/driving_response.dart';
 import 'package:maps_app_flutter/model/search_response.dart';
 
@@ -16,6 +19,11 @@ class TrafficService{
   }
 
   final _dio = new Dio();
+  final debouncer = Debouncer<String>(duration: Duration(milliseconds: 400 ));
+   
+  final StreamController<SearchResponse> _sugerenciasStreamController = new StreamController<SearchResponse>.broadcast();
+  Stream<SearchResponse> get sugerenciasStream => this._sugerenciasStreamController.stream;
+
   final _baseUrlManual = 'https://api.mapbox.com/directions/v5'; /// se usa manualmente con un punto LatLng de destino
   final _baseUrlRequest = 'https://api.mapbox.com/geocoding/v5'; // se envía el nombre de un lugar como destino
   final _apikey = access_token;
@@ -40,17 +48,37 @@ class TrafficService{
   }
 
   Future<SearchResponse> getResultadosRequest(String busqueda, LatLng miUbicacion) async{
-    //print('${miUbicacion.longitude},${miUbicacion.latitude}');
+    
     final url = '${this._baseUrlRequest}/mapbox.places/$busqueda.json';
-    final resp = await this._dio.get(url, queryParameters: {
+    try {
+      final resp = await this._dio.get(url, queryParameters: {
       'access_token': this._apikey,
       'autocomplete':true,
       'proximity'   : '${miUbicacion.longitude},${miUbicacion.latitude}',
       'language'    :'es'
     });
-    //print(resp.data);
+    
     final searchResponse = searchResponseFromJson(resp.data);
     
     return searchResponse;
+    } catch (e) {
+      return SearchResponse( features: [] );
+    }
   }
+
+  void getSugerenciasPorQuery( String busqueda, LatLng proximidad ) {
+
+  debouncer.value = '';
+  debouncer.onValue = ( value ) async {
+    final resultados = await this.getResultadosRequest(value, proximidad);
+    this._sugerenciasStreamController.add(resultados);
+  };
+
+  final timer = Timer.periodic(Duration(milliseconds: 200), (_) {
+    debouncer.value = busqueda;
+  });
+
+  Future.delayed(Duration(milliseconds: 201)).then((_) => timer.cancel()); 
+
+}
 }
